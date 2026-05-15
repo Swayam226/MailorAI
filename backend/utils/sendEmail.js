903f3512ee737f8b2c43ecf768793e090b3d60f5
Swayam <swayamkumar226@gmail.com>
2026-05-15 08:02:03 +0000

    fixed smtp and proxy config

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,16 +1,30 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    },
+
+    family: 4
+});
+
 const sendEmail = async (options) => {
+
     try {
-        if (!process.env.EMAIL_USER || !process.env.EMAIL_PASS) {
-            throw new Error("Email credentials are not set in ENV variables");
+
+        if (
+            !process.env.EMAIL_USER ||
+            !process.env.EMAIL_PASS
+        ) {
+            throw new Error(
+                "Email credentials are not set in ENV variables"
+            );
         }
-        const transporter = nodemailer.createTransport({
-            service: "Gmail",
-            auth: {
-                user: process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASS
-            }
-        });
 
         const mailOptions = {
             from: process.env.EMAIL_USER,
@@ -18,13 +32,20 @@ const sendEmail = async (options) => {
             subject: options.subject,
             text: options.text
         };
+
         await transporter.sendMail(mailOptions);
-        console.log("Email sent succesfully")
+
+        console.log("Email sent successfully");
 
     } catch (error) {
-        console.log("Error sending email", error.message);
+
+        console.log(
+            "Error sending email",
+            error.message
+        );
+
         throw error;
     }
-}
+};
 
 module.exports = sendEmail;
